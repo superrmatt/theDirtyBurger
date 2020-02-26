d57eb399c73c3b2a0c534afa5fbd05a75ddd131c
Matt Dambra <mpdambra@gmail.com>
2020-02-25 21:27:57 -0500

    minor change to seeds.sql file

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,4 +2,4 @@ use burgers;
 
 insert into burgers (burger_name, devoured) values ("big mac", false);
 insert into burgers (burger_name, devoured) values ("whopper", false);
-insert into burgers (burger_name, devoured) values ("Turkey Burger", false);
+insert into burgers (burger_name, devoured) values ("DIrty Burger", false);
